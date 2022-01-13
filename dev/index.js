3148a1325a2c3c2d4a37bd3384610abba268f120
gustavo <gusfer@gmail.com>
2022-01-13 14:02:04 +0100

    Added dropdown to change the number of verses in context

--- dev/index.js
+++ dev/index.js
@@ -8,12 +8,18 @@ function updateTexts(){
             var witid = $(this).attr('id');
             var content_main = verses['content'][witid][selectedVersNummer];
             var orderList = verses['order'].filter(y => y['wit'] == witid);
-            var prevVersNumber = getContextVers(orderList, selectedVersNummer, -1);
-            var nextVersNumber = getContextVers(orderList, selectedVersNummer, +1);
-            var content_prev = verses['content'][witid][prevVersNumber];
-            var content_next = verses['content'][witid][nextVersNumber];
+            var distance = $('input[name=contextSelCheck]:checked').val();
+            var content_prev = '';
+            var content_next = '';
+            for (var i=1; i <= distance; i++){
+                var prevVersNumber = getContextVers(orderList, selectedVersNummer, -i);
+                var nextVersNumber = getContextVers(orderList, selectedVersNummer, +i);
+                content_prev += verses['content'][witid][prevVersNumber] + ' / ';
+                content_next += ' / ' + verses['content'][witid][nextVersNumber];    
+            }
+            
             // var content_prev = verses['content'][witid][54];
-            console.log(content_prev, content_next);
+            
             // var prevVersNummer = verses['order'][witid];
             // var prevContent = verses['content'][witid][prevVersNummer];
             if (typeof content_main == 'undefined'){
@@ -27,13 +33,13 @@ function updateTexts(){
             }
             $('#texte').append('<div class="col-md-12 col-sm-12">'+
                         '<div class="witlabel">'+ witid.replace('.xml', '').replace('_', ' ').substr(0,13) +'</div>'+
-                        '<div class="verscontent">'+content_prev + ' / <strong style="font-size:larger">' + content_main + '</strong> /' + content_next + '</div></div>')
+                        '<div class="verscontent">'+content_prev + ' <strong style="font-size:larger">' + content_main + '</strong> ' + content_next + '</div></div>')
         }
     })
 }
 
 function getContextVers(orderList, current, distance){
-    console.log(orderList[0]['order'], current, distance);
+    // console.log(orderList[0]['order'], current, distance);
     // var currIntPos = orderList[0]['order'].filter(e => );
     var contextVersGenNum = -1
     $.each(orderList[0]['order'], function(key, value) { 
@@ -48,10 +54,8 @@ function getContextVers(orderList, current, distance){
 }
 
 $(document).ready(function(){
-
-    
-
-    
+  
+    // Create the list of witnesses
     for (var i = 0; i < verses['order'].length; i++){
         var witfull = verses['order'][i]['wit'];
         var witclean = witfull.replace('_', ' ').replace('.xml', '');
@@ -59,9 +63,11 @@ $(document).ready(function(){
         $('#witsel').append('<div class="col-md-3 col-sm-6"><label class="witsellabel">'+ witclean +'</label>'+
                             '<input class="witselcheck" type="checkbox" id="'+witfull +'" name="wit_'+witsigle+'" ></div>') 
     }
-
     $('#witsel').collapse();
+    $('input[name=contextSelCheck][value="1"]').prop("checked", true);
 
+
+    // Enter to update
     $(document).keydown(function (e) {
         if (e.which == 13) {
             if ($('#versn').is(":focus")){
@@ -72,6 +78,7 @@ $(document).ready(function(){
     });
     
     $('#updateVers').on('click', function(){updateTexts()});
+    $('.witselcheck').change(function(){updateTexts()});
     
     $('#prev').on('click',function () {
         $('#versn').val(parseInt($('#versn').val()) - 1);
@@ -81,5 +88,10 @@ $(document).ready(function(){
         $('#versn').val(parseInt($('#versn').val()) + 1);
         updateTexts();
       })
+
+    
+    $('input[name=contextSelCheck]').change(function(){
+        updateTexts();
+    })
 
 })
